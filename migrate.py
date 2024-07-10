d0ba982d28a48a20fdf2444b37f077133e84e6c8
Andres <00305422@uca.edu.sv>
2024-07-10 11:38:36 -0600

    Drive2

--- migrate.py
+++ migrate.py
@@ -9,6 +9,6 @@ def clear_table(table_name):
     print(f'Table {table_name} has been cleared.')
 
 if __name__ == '__main__':
-    tables_to_clear = ['notifications', 'documents', 'shared_files']
+    tables_to_clear = ['feedback']
     for table in tables_to_clear:
         clear_table(table)
